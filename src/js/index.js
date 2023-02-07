517bd1a2c831e2f26248e5523535c3344ecc0770
Jeferson <jefersonlopeseugenio11@gmail.com>
2023-02-07 12:38:07 -0300

    Coloquei tabela, cores css e coloquei nome e pontuação

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,53 @@ var cartoes = document.querySelectorAll(".cartao");
 var existeCartaoVirado = false;
 var primeiroCartao, segundoCartao;
 
+var resultadosJogadores = [];
+
+var itensStorage = localStorage.getItem("resultado");
+
+if (itensStorage) {
+  var resultadosExistentes = JSON.parse(itensStorage);
+} else {
+  var resultadosExistentes = [];
+}
+
+console.log(resultadosJogadores);
+
 var placar = 0;
-var tempo = 5;
+var tempo = 15;
 var tempos = document.getElementById("tempo");
 
 var intervalo = setInterval(function() {
   if ( tempo == 0) {
     tempos.style.color = 'red';
     clearInterval(intervalo);
+    alert("Tempo esgotado!");
+    var nome = prompt("Digite o seu nome:");
+    //Criar um objeto
+    var resultado = {
+      nomeJogador: nome,
+      pontuacao: placar,
+    };
+
+    resultadosExistentes.push(resultado);
+    localStorage.setItem("resultado", JSON.stringify(resultadosExistentes));
+
+    document.getElementById("recordes").style.display = "block";
+
+    var tabela = document.getElementById("resultados");
+
+    resultadosExistentes.sort().forEach(function (resultado) {
+      var tr = document.createElement("tr");
+      var tdNome = document.createElement("td");
+      tdNome.innerHTML = resultado.nomeJogador;
+      var tdPontuacao = document.createElement("td");
+      tdPontuacao.innerHTML = resultado.pontuacao;
+      tr.appendChild(tdNome);
+      tr.appendChild(tdPontuacao);
+      tabela.appendChild(tr);
+    });
     cartoes.forEach(function (cartao) {
-      cartao.addEventListener("click", virarCartao);
+      cartao.removeEventListener("click", virarCartao);
     });
     return;
   }
